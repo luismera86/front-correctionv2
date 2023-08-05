fc0ec97e26912e680a9b8de9e25a55697fd78348
Luis Mera <luismera86@gmail.com>
2023-08-04 21:52:35 -0300

    .

--- src/pages/admin/AddCourse.tsx
+++ src/pages/admin/AddCourse.tsx
@@ -6,20 +6,24 @@ interface IFormState {
   courseName: string;
 }
 
-
 export const AddCourse = () => {
+  const { onInputChange, formState } = useForm<IFormState>({
+    courseName: "",
+  });
 
-  const { onInputChange, onReset, formState } = useForm<IFormState>({
-    courseName: ''
-  })
-
-  const { courseName } = formState
+  const { courseName } = formState;
 
   return (
     <Layout>
       <BoxPaper spaceY={2}>
         <label>Nombre del curso</label>
-        <input name="courseName" value={courseName} onChange={onInputChange} className="bg-[#44464e]  text-[#E5F876] p-2" type="text" />
+        <input
+          name="courseName"
+          value={courseName}
+          onChange={onInputChange}
+          className="bg-[#44464e]  text-[#E5F876] p-2"
+          type="text"
+        />
         <Button name="Agregar" />
       </BoxPaper>
     </Layout>

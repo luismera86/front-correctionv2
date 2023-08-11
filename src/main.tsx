e2caa8035383de4e6b4bd768980fd4360000bdba
Luis Mera <luismera86@gmail.com>
2023-08-11 13:21:05 -0300

    React-query instalado y configurado el provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { AppRouter } from "./router/AppRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={AppRouter} />Z
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={AppRouter} />Z
+    </QueryClientProvider>
   </React.StrictMode>
 );
